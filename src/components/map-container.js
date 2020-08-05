cf1761f57a0c70f33e85cc3936d1572ec933a5be
lalinda74 <diaslalinda@gmail.com>
2020-08-05 18:34:29 +0530

    Map API changes

--- src/components/map-container.js
+++ src/components/map-container.js
@@ -1,26 +1,42 @@
 import React from 'react';
-import { GoogleMap, withScriptjs, withGoogleMap } from 'react-google-maps';
+import { GoogleMap, useLoadScript, Marker, InfoWindow } from '@react-google-maps/api';
+import mapStyles from '../styles/map-styles';
 
-function Map() {
-    return (
-        <GoogleMap defaultZoom={10} defaultCenter={{ lat: 19.075983, lng: 72.877655}} />
-    );
+const mapContainerStyle = {
+    width: "100vw",
+    height: "100vh"
 }
-
-const WrappedMap = withScriptjs(withGoogleMap(Map));
+const center = {
+    lat: 6.927079,
+    lng: 79.861244
+}
+const options = {
+    styles: mapStyles,
+    disableDefaultUI: true,
+    zoomControl: true
+}
 
 export default function MapContainer() {
 
-    const map_key = 'AIzaSyB1RE8whFkW6IGyJDICc-f-E88xq9tQL9s';
+    const map_key = 'AIzaSyB1RE8whFkW6IGyJDICc-f-E88xq9tQL9s98';
 
+    const { isLoaded, loadError } = useLoadScript({
+        googleMapsApiKey: map_key
+    });
+
+    if (loadError)
+        return "Error loading maps";
+    if (!isLoaded)
+        return "Loading maps";
+                     
     return (
         <div style = {{width: '100%', height: '95vh'}}>
-            <WrappedMap
-                googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${map_key}`}
-                loadingElement={<div style={{ height: `100%` }} />}
-                containerElement={<div style={{ height: `100%` }} />}
-                mapElement={<div style={{ height: `100%` }} />}
-            />
+            <GoogleMap 
+                mapContainerStyle={mapContainerStyle} 
+                zoom={12} 
+                center={center}
+                options={options}>
+            </GoogleMap>
         </div>
     );
 }
