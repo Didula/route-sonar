3b1ca3cbede4ef5f4b39e55259899fe3bd43628e
thish1991 <thishanpavithra@gmail.com>
2020-08-12 02:00:03 +0530

    Home page styling done (not responsive)

--- src/components/Home/FloatingBox/FloatingBox.js
+++ src/components/Home/FloatingBox/FloatingBox.js
@@ -1,12 +1,27 @@
 import React from 'react';
 
 import classes from './FloatingBox.module.css';
+import locPin from '../../../assets/pin_icon.png';
+
 import LocInput from '../../../UI elements/LocInput/LocInput';
+import Btn from '../../../UI elements/Button/Btn';
 
 const fBox = () => (
    <div className={classes.FBox}>
        <h4>Where are you <span style={{color:'#b60000'}}>headed?</span></h4>
        <LocInput text='Select Location'/>
+       <hr/>
+       <div>
+            <img src={locPin} alt='+'/>
+            <p>Route Details</p>
+       </div>
+       <div>
+            <Btn clicked='/' btnType='Main'>Add Locations</Btn>
+            <Btn clicked='/' btnType='Secondary'>Upload Locations</Btn>
+       </div>
+       <div>
+           <p>Need any assistance? <a href='#'>Visit F&Q Section</a></p>
+       </div>
    </div>
 );
 
