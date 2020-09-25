85d4d84a7205b715a9146c5dc843f2427c248d15
devo.dce@gmail.com <d!dula1991>
2020-09-25 17:19:20 +0530

    Restructuring

--- src/components/UI/Buttons/GenericButton/GenericButton.js
+++ src/components/UI/Buttons/GenericButton/GenericButton.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import classes from './Btn.module.css';
+import classes from './GenericButton.module.css';
 
 const genericButton = (props) => (
     <button className={[classes.Btn, classes[props.btnType]].join(' ')} onClick={props.clicked}>
