b313542417c6cacb4092ddf4343785ef4b6df069
Didula <devo.dce@gmail.com>
2020-10-11 15:24:24 +0530

    Adding store actions and reducers for login modal show state control.

--- src/store/reducers/authReducer.js
+++ src/store/reducers/authReducer.js
@@ -1,13 +1,22 @@
+import {updateObject} from "../../shared/utility";
+import * as actionTypes from "../actions/actionTypes";
+
 const initialState = {
-    token: null,
+    isLoginModalOpen: false,
     userId: null,
     error: false,
     loading: false,
     user: null
 }
 
+const setLoginModalOpen = (state, action) => {
+    return updateObject(state, {isLoginModalOpen: action.flag})
+}
+
 const authReducer = (state = initialState, action) => {
     switch (action.type) {
+        case actionTypes.SET_LOGIN_MODAL_OPEN:
+            return setLoginModalOpen(state, action);
         default:
             return state;
     }
