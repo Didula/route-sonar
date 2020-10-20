d28edb4e7f08cd8dfd5b68692b85316463dd517a
Didula <devo.dce@gmail.com>
2020-10-20 10:23:55 +0530

    Fixing rounding

--- src/store/sagas/homeSaga.js
+++ src/store/sagas/homeSaga.js
@@ -2,6 +2,7 @@ import {call} from 'redux-saga/effects';
 import axios from "axios";
 
 const CONSUMPTION_SAVE_END_POINT = 'consumptionAdd'
+const ROUTE_SAVE_END_POINT = 'saveOpt'
 const optimizedLocation = {
     "distance_from_last_point": 0,
     "eta": "01/01/2000 00:00:00",
@@ -32,8 +33,8 @@ export function* saveOptimizedRouteSaga(action) {
         "customer_id": action.customerId,
         "user_id": action.userId,
         "trip_id": yield call(generateUUID),
-        "vehicle_id": action.vehicleId,
-        "driver_id": action.driverId,
+        "vehicle_id": "didula",
+        "driver_id": "12345",
         "start_time": "01/01/2000 00:00:00",
         "end_time": "01/01/2000 00:00:00",
         "optimized_distance": 0,
@@ -43,7 +44,7 @@ export function* saveOptimizedRouteSaga(action) {
         "optLocations": []
     }
     getDataFromDirection(action.direction,routeSaveRequest)
-    let url = process.env.REACT_APP_API_URL + CONSUMPTION_SAVE_END_POINT;
+    let url = process.env.REACT_APP_API_URL + ROUTE_SAVE_END_POINT;
     try {
         axios.defaults.headers.post['Content-Type'] = 'application/json';
         const response = yield axios.post(url, routeSaveRequest);
@@ -64,38 +65,45 @@ function getDataFromDirection(direction,routeSaveRequest) {
     let optLocations = [];
     let currentTime = new Date();
     let endTime = new Date();
-    let currentTimeString = `${
-        currentTime.getDate().toString().padStart(2, '0')}/${
-        (currentTime.getMonth()+1).toString().padStart(2, '0')}/${
-        currentTime.getFullYear().toString().padStart(4, '0')} ${
-        currentTime.getHours().toString().padStart(2, '0')}:${
-        currentTime.getMinutes().toString().padStart(2, '0')}:${
-        currentTime.getSeconds().toString().padStart(2, '0')}`
+    let currentTimeString = getDateTimeString(currentTime)
     if (direction) {
-        direction.routes[0].legs.forEach(leg => {
+        direction.routes[0].legs.forEach((leg,index) => {
             totalDistance += leg.distance.value;
             totalTime += leg.duration.value;
 
             let locationClone = {...optimizedLocation};
-            locationClone.distance_from_last_point = leg.distance.value/1000;
+            let eta = new Date();
+            eta.setSeconds(eta.getSeconds() + leg.duration.value)
+            locationClone.distance_from_last_point = round(leg.distance.value/1000,1);
             locationClone.lat = leg.end_location.lat();
             locationClone.lon = leg.end_location.lng();
             locationClone.place = leg.end_address;
+            // todo fix this order of points.
+            locationClone.order = index;
+            locationClone.eta = getDateTimeString(eta);
             optLocations.push(locationClone);
         })
     }
-    endTime.setMinutes(endTime.getMinutes() + totalTime/60)
-    let endTimeString = `${
-        endTime.getDate().toString().padStart(2, '0')}/${
-        (endTime.getMonth()+1).toString().padStart(2, '0')}/${
-        endTime.getFullYear().toString().padStart(4, '0')} ${
-        endTime.getHours().toString().padStart(2, '0')}:${
-        endTime.getMinutes().toString().padStart(2, '0')}:${
-        endTime.getSeconds().toString().padStart(2, '0')}`
-    routeSaveRequest.optimized_distance = totalDistance/1000;
-    routeSaveRequest.estimated_distance = totalDistance + totalDistance/1000 * 0.15;
+    endTime.setMinutes(endTime.getMinutes() + Math.round(totalTime/60))
+    let endTimeString = getDateTimeString(endTime)
+    routeSaveRequest.optimized_distance = round(totalDistance/1000,1);
+    routeSaveRequest.estimated_distance = round(totalDistance/1000 * 1.15,1);
     routeSaveRequest.start_time = currentTimeString;
     routeSaveRequest.end_time = endTimeString;
-    routeSaveRequest.total_minutes = totalDistance/60;
+    routeSaveRequest.total_minutes = Math.round(totalDistance/60);
     routeSaveRequest.optLocations = optLocations;
+}
+
+function round(value, decimals) {
+    return Number(Math.round(value+'e'+decimals)+'e-'+decimals);
+}
+
+function getDateTimeString(date) {
+    return `${
+        date.getDate().toString().padStart(2, '0')}/${
+        (date.getMonth()+1).toString().padStart(2, '0')}/${
+        date.getFullYear().toString().padStart(4, '0')} ${
+        date.getHours().toString().padStart(2, '0')}:${
+        date.getMinutes().toString().padStart(2, '0')}:${
+        date.getSeconds().toString().padStart(2, '0')}`
 }
